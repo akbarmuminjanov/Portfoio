4be001d25b7874315adff4896bd098824bcb7663
akbarmuminjanov <muminjanov.75@gmail.com>
2024-05-04 15:25:44 +0500

    fix

--- resume/urls.py
+++ resume/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index, resume
+from .views import index, resume, portfolio, blog
 
 urlpatterns = [
     path('', index, name="home"),
-    path('resume/', resume, name="resume")
+    path('resume/', resume, name="resume"),
+    path('portfolio/', portfolio, name="portfolio"),
+    path('blog/', blog, name="blog"),
 ]
--- resume/views.py
+++ resume/views.py
@@ -10,3 +10,11 @@ def index(request):
 def resume(request):
     resume = Resume.objects.get()
     return render(request, "resume.html", {"resume":resume})
+
+def portfolio(request):
+    resume = Resume.objects.get()
+    return render(request, "portfolio.html", {"resume":resume})
+
+def blog(request):
+    resume = Resume.objects.get()
+    return render(request, "blog.html", {"resume":resume})
